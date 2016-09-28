12fde770d20e39a779391924d7acb702ac344b51
rouz majlessi <rmajlessi@gmail.com>
2016-09-28 11:29:19 -0700

    write first test for empty Database for Venues

---
+++ Tests/VenueTest.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class VenueTest : IDisposable
+  {
+    public VenueTest()
+    {
+       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test1_EmptyDatabase()
+    {
+      int resut = Venue.GetAl().Count;
+
+      Assert.Equal(0,result);
+    }
+  }
+}
